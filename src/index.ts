9825f5139030566a25785f766f0a85dd993507db
Steffen Schaffert <s.schaffert@student.maastrichtuniversity.nl>
2017-01-18 16:19:07 +0100

    Example for twitter call, added api keys

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-var ajax = require('request-promise-native');
+/*var ajax = require('request-promise-native');
 
 var options = {
     uri: 'https://moped.ecampus.rwth-aachen.de/proxy/api/v2/version',
@@ -10,4 +10,15 @@ var options = {
 
 ajax(options).then((response) => {
     console.log(response.Data);
+});*/
+var twit = require('twit');
+
+var T = new twit({
+    consumer_key: 'gP2Tuc7uygn95weeVzwlj6A2b',
+    consumer_secret: 'o9onFEm6Oqvamz9ohPwx4UEPKmjRiyMe3vc7le7qCJLW6CvKPs',
+    app_only_auth: true
+});
+
+T.get('search/tweets', { q: 'banana since:2016-08-13', count: 100 }, function(err, data, response) {
+  console.log(data)
 });
