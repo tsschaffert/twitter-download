b49e0405475ae906fd58f9b6ac86dcbd849c24b3
Steffen Schaffert <s.schaffert@student.maastrichtuniversity.nl>
2017-01-18 19:20:21 +0100

    First approach to search and list tweets

--- src/TwitWrapper.ts
+++ src/TwitWrapper.ts
@@ -14,8 +14,46 @@ export default class TwitWrapper {
         this.T = new twit(config);
     }
 
-    public search(query: string, count: number): Promise<any> {
-        return this.T.get('search/tweets', { q: query, count: count }).then((result) => {
+    public search(query: string, count: number = 100, maxId?: string, lang: string = 'en'): Promise<{ 
+        statuses: {
+            created_at: string,
+            id_str: string,
+            text: string,
+            retweeted: boolean,
+            user: {
+                id: number,
+                id_str: string,
+                screen_name: string
+            }
+        }[],
+        search_metadata: {
+            max_id_str: string,
+            query: string,
+            count: number
+        }
+    }> {
+        return this.T.get('search/tweets', { q: query, count: count, lang: lang, max_id: maxId }).then((result) => {
+            return result.data;
+        });
+    }
+
+    public getTimeline(user: string | number, count: number = 200, maxId?: number, excludeReplies: boolean = true, includeRts: boolean = false, trimUser: boolean = true): Promise<{
+        created_at: string,
+        id_str: string,
+        text: string,
+        user: {
+            id: number,
+            id_str: string
+        }
+    }[]> {
+        let userId: number = null;
+        let screenName: string = null;
+        if (typeof user === 'number') {
+            userId = user;
+        } else {
+            screenName = user;
+        }
+        return this.T.get('statuses/user_timeline', { user_id: userId, screen_name: screenName, count: count, max_id: maxId, exclude_replies: excludeReplies, include_rts: includeRts, trim_user: trimUser }).then((result) => {
             return result.data;
         });
     }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import twitWrapper from "./TwitWrapper";
 
-var T = new twitWrapper({
-    consumer_key: 'gP2Tuc7uygn95weeVzwlj6A2b',
-    consumer_secret: 'o9onFEm6Oqvamz9ohPwx4UEPKmjRiyMe3vc7le7qCJLW6CvKPs',
-    app_only_auth: true
-});
+async function test() {
+    var T = new twitWrapper({
+        consumer_key: 'gP2Tuc7uygn95weeVzwlj6A2b',
+        consumer_secret: 'o9onFEm6Oqvamz9ohPwx4UEPKmjRiyMe3vc7le7qCJLW6CvKPs',
+        app_only_auth: true
+    });
 
-T.search('banana since:2016-08-13', 100).then((data) => {
-    console.log(data);
-});
+    let results = await T.search('have been hacked', 10);
+
+    for (let result of results.statuses) {
+        if(!result.retweeted) {
+            console.log(`** ${result.text} **`);
+            let tweets = await T.getTimeline(result.user.screen_name, 10);
+            for (let tweet of tweets) {
+                console.log(tweet.text.trim());
+            }
+            console.log("\n");
+        }
+    }
+}
+
+test();
